40e934274423a0674f4853e720dc5778c67bdec6
Mikimoto <mikimoto.c@gmail.com>
2013-04-30 05:02:20 +0800

    add grap script
    Signed-off-by: Mikimoto <mikimoto.c@gmail.com>

---
+++ scraper/grap.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#export DATE=`date +%m%d`
+#curl -L www.yahoo.com.tw -# -o $DATE
+curl -L http://www.cnyes.com/futures/flashchart/DUBS.html -# -o `date +%Y%m%d`Dubai
+curl -L http://www.cnyes.com/futures/flashchart/BREN.html -# -o `date +%Y%m%d`Brent
+curl -L http://www.bloomberg.com/quote/USDTWD:CUR -# -o `date +%Y%m%d`USD-TWD
