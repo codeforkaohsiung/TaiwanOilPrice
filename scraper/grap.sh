3e7b52042f391b953a7cba2c3cd6f1bfce1b85b1
Evan <evan@mobilogics.com.tw>
2013-05-07 18:52:03 +0800

    add remark

--- scraper/grap.sh
+++ scraper/grap.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-#export DATE=`date +%m%d`
-#curl -L www.yahoo.com.tw -# -o $DATE
+#Dubai
 curl -L http://www.cnyes.com/futures/flashchart/DUBS.html -# -o `date +%Y%m%d`Dubai
+
+#Brent
 curl -L http://www.cnyes.com/futures/flashchart/BREN.html -# -o `date +%Y%m%d`Brent
+
+#USDTWD
 curl -L http://www.bloomberg.com/quote/USDTWD:CUR -# -o `date +%Y%m%d`USD-TWD
