c7e9659e303f6c1863c27f6a0951f2dde54e11e6
Evan Wu <evan3rd@gmail.com>
2014-03-04 10:12:17 +0800

    download page and parser

---
+++ parserBrent.pl
@@ -0,0 +1,32 @@
+my $s;
+
+open(HTM, '20140225Brent') or die "$!";
+while (defined($line = <HTM>)) {
+	$s = $s.$line;
+}
+close(HTM);
+
+print "\n========================\n";
+
+
+my $last = "昨收";
+my $range = index($s, $last);
+#print ($range);
+
+#print "\n\n";
+
+#print substr($s, $range + 30, 8);
+
+my $date = substr($s, $range + 30, 8);
+
+#print "\n\n";
+
+#print substr($s, $range + 57, 6);
+
+my $price = substr($s, $range + 57, 6);
+
+my $tar = "Brent ".$date." ".$price;
+
+print $tar;
+print "\n========================\n";
+print "\n\n";
